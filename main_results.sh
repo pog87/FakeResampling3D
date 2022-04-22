506c23d80a7e5760f97cb1ee9370213ac2279f91
pog87 <pog87@email.it>
2022-04-22 10:48:44 +0200

    lil add

--- main_results.sh
+++ main_results.sh
@@ -1,14 +1,19 @@
-echo "mean value per segment"
+echo "Mean value per segment"
 echo "gold standard"
 python mean_per_segment.py data/SL_hq.nii.gz data/SL_hq_seg.nii.gz
 
-echo "resampling"
+echo "Trilinear upsampling"
 python mean_per_segment.py results/SL_hq_reg.nii.gz data/SL_hq_seg.nii.gz
 
-echo "Fake Nodes resampling"
+echo "Fake Nodes + Trilinear upsampling"
 python mean_per_segment.py results/SL_hq_reg_fake_unfake.nii.gz data/SL_hq_seg.nii.gz
 
-echo "global error measures"
+echo "Downscaling the segments"
+python mean_per_segment.py data/SL_lq.nii.gz results/SL_lq_seg.nii.gz
+
+echo "------------"
+
+echo "Global error measures"
 echo "gold standard Vs goal standard"
 python global_error_measures.py data/SL_hq.nii.gz data/SL_hq.nii.gz
 
